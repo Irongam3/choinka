c5929317ace2a6d27bc08a3647b006205b6ec0f8
Piotr Bielecki <hfx.music@gmail.com>
2018-07-14 21:30:59 +0200

    added JS file

---
+++ choinka.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function drawTree (i) {
+	for ( var i = 0; i < 10; i++;) {
+		var star = "";
+		for ( var j = 0; j < 10; j++) {
+			var star += '*';
+		}
+	}
+}
+
+console.log(star)
